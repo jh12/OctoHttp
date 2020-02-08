1304afe3efbc995f0ba0f9b26a9dcc0e8d2afd9c
jh12 <jabaha35@hotmail.com>
2020-02-08 11:04:41 +0100

    Simple GetJsonAsync implementation and tests

---
+++ OctoHttp.UnitTests/Extensions/HttpClientExtensionsTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using OctoHttp.Extensions;
+
+namespace OctoHttp.UnitTests.Extensions
+{
+    [TestFixture]
+    public class HttpClientExtensionsTests
+    {
+        private HttpClient CreateMockClient(HttpStatusCode responseStatusCode, object content)
+        {
+            Mock<HttpMessageHandler> handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+
+            string jsonContent = JsonConvert.SerializeObject(content);
+
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                    )
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = responseStatusCode,
+                    Content = new StringContent(jsonContent)
+                })
+                .Verifiable();
+
+            HttpMessageHandler handler = handlerMock.Object;
+
+            return new HttpClient(handler);
+        }
+
+        [Test]
+        public async Task GetJsonAsync_ValidResponse_Pass()
+        {
+            // Arrange
+            object response = new {key = 1, value = "string"};
+            HttpClient httpClient = CreateMockClient(HttpStatusCode.OK, response);
+
+            // Act
+            KeyValuePair<int, string> result = await httpClient.GetJsonAsync<KeyValuePair<int, string>>(new Uri("http://unit.test"));
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Key.Should().Be(1);
+            result.Value.Should().Be("string");
+        }
+
+        [Test]
+        public async Task GetJsonAsyncWithString_ValidResponse_Pass()
+        {
+            // Arrange
+            object response = new {key = 1, value = "string"};
+            HttpClient httpClient = CreateMockClient(HttpStatusCode.OK, response);
+
+            // Act
+            KeyValuePair<int, string> result = await httpClient.GetJsonAsync<KeyValuePair<int, string>>("http://unit.test");
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Key.Should().Be(1);
+            result.Value.Should().Be("string");
+        }
+
+        [Test]
+        public async Task GetJsonAsync_BadRequest_ShouldThrowException()
+        {
+            // Arrange
+            object response = new {key = 1, value = "string"};
+            HttpClient httpClient = CreateMockClient(HttpStatusCode.BadRequest, response);
+
+            // Act
+            Func<Task> act = () => httpClient.GetJsonAsync<KeyValuePair<int, string>>(new Uri("http://unit.test"));
+
+            // Assert
+            (await act.Should().ThrowExactlyAsync<HttpRequestException>())
+                .And.Message.Should().EndWith("400 (Bad Request).");
+        }
+    }
+}
---
+++ OctoHttp/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OctoHttp.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        internal static JsonSerializer JsonSerializer = JsonSerializer.CreateDefault();
+
+        public static Task<T> GetJsonAsync<T>(this HttpClient httpClient, Uri requestUri, CancellationToken cancellationToken = default)
+        {
+            return GetResponseJsonAsync<T>(httpClient, HttpMethod.Get, requestUri, null, cancellationToken);
+        }
+
+        public static Task<T> GetJsonAsync<T>(this HttpClient httpClient, string requestUri, CancellationToken cancellationToken = default)
+        {
+            if(requestUri == null)
+                throw new ArgumentNullException(nameof(requestUri));
+
+            return GetJsonAsync<T>(httpClient, CreateUri(requestUri), cancellationToken);
+        }
+
+        private static async Task<T> GetResponseJsonAsync<T>(HttpClient client, HttpMethod method, Uri requestUri, object? content = default, CancellationToken cancellationToken = default)
+        {
+            using (HttpRequestMessage request = new HttpRequestMessage(method, requestUri))
+            {
+                if (content != null)
+                    request.AddJsonContent(content);
+
+                using (HttpResponseMessage response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    return await response.Content.ReadAsJsonAsync<T>();
+                }
+            }
+        }
+
+        public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
+        {
+            using Stream stream = await content.ReadAsStreamAsync();
+            using StreamReader reader = new StreamReader(stream);
+            using JsonReader jsonReader = new JsonTextReader(reader);
+
+            return JsonSerializer.Deserialize<T>(jsonReader);
+        }
+
+        private static void AddJsonContent(this HttpRequestMessage request, object content)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            using (StreamWriter writer = new StreamWriter(stream))
+            using (JsonTextWriter jsonWriter = new JsonTextWriter(writer))
+            {
+                JsonSerializer.Serialize(jsonWriter, content);
+
+                StreamContent streamContent = new StreamContent(stream);
+
+                request.Content = streamContent;
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                request.Content.Headers.ContentEncoding.Add(Encoding.UTF8.ToString());
+            }
+        }
+
+        private static Uri CreateUri(string uri)
+        {
+            return new Uri(uri, UriKind.RelativeOrAbsolute);
+        }
+    }
+}
